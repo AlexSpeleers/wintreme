e359654a3e51f041f50d12ece0de2ea4a4a115ab
BRISINGR <alexsieger@gmail.com>
2025-10-19 19:00:52 +0200

    set stirpe prod key

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,6 +3,5 @@ export const environment = {
   apiUrl: 'api/',
   hubUrl: 'hub/notifications',
   stripePublicKey:
-    'pk_live_51RwqJyR9WcmHDZidiPWnWF4cGTSN0QLEUHLlDlot2LgcZETXMAfZYYip4pBVjxj7QdtXmr0LGTcpkvQdEswWovv300URXG7kXj',
+    'pk_live_51S8OfCBTmJC4myOXb3lbHzRVnKVETI0zwIxsuNUgqou7kxeB5JoCVlrZTRN3T5IzvWaQJqjAuHWjFqjgGqjfZWXt00DaZ4poIU',
 };
-//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
